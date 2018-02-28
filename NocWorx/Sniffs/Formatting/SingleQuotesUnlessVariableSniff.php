2b1f0dbe475f747c62f4c4de2e01b0ad3c14c577
Cal Evans <cevans@nexcess.net>
2018-02-28 09:22:54 -0500

    Cosmetic and added in other escape values

--- NocWorx/Sniffs/Formatting/SingleQuotesUnlessVariableSniff.php
+++ NocWorx/Sniffs/Formatting/SingleQuotesUnlessVariableSniff.php
@@ -8,59 +8,62 @@ class SingleQuotesUnlessVariableSniff implements Sniff
 {
 
 
-    /**
-     * Returns the token types that this sniff is interested in.
-     *
-     * @return array(int)
-     */
-    public function register()
-    {
-        return [T_CONSTANT_ENCAPSED_STRING];
-
-    }//end register()
+  /**
+   * Returns the token types that this sniff is interested in.
+   *
+   * @return array(int)
+   */
+  public function register() {
+    return [T_CONSTANT_ENCAPSED_STRING];
+  }//end register()
 
 
-    /**
-     * Processes this sniff, when one of its tokens is encountered.
-     *
-     * @param \PHP_CodeSniffer\Files\File $phpcsFile The current file being
-     *  checked.
-     * @param int $stackPtr The position of the current token in the
-     *  stack passed in $tokens.
-     *
-     * @return void
-     */
-    public function process(File $phpcsFile, $stackPtr)
-    {
-        $tokens = $phpcsFile->getTokens();
+/**
+ * Processes this sniff, when one of its tokens is encountered.
+ *
+ * @param \PHP_CodeSniffer\Files\File $phpcsFile The current file being
+ *  checked.
+ * @param int $stackPtr The position of the current token in the
+ *  stack passed in $tokens.
+ *
+ * @return void
+ */
+public function process(File $phpcsFile, $stackPtr) {
+  $tokens = $phpcsFile->getTokens();
 
-        if ($tokens[$stackPtr]['type'] === 'T_CONSTANT_ENCAPSED_STRING') {
-            
-            // if it starts with a double quote
-            if ($tokens[$stackPtr]['content']{0} ==='"') {
-                // ...and contains a single quote, we're good.
-                if (strpos($tokens[$stackPtr]['content'] , "'")) {
-                    return;
-                }
+  if ($tokens[$stackPtr]['type'] === 'T_CONSTANT_ENCAPSED_STRING') {
 
-                // ...and contains a carrage return
-                if (strpos($tokens[$stackPtr]['content'] , '\n')) {
-                    return;
-                }
-            
-                // Otherwise, it's an error.
-                $error = 'Only use Double Quotes when a variable is being ' .
-                    'inserted.';
-                $data = [trim($tokens[$stackPtr]['content'])];
-                
-                $phpcsFile->addError(
-                    $error,
-                    $stackPtr,
-                    'FOUND',
-                    $data
-                );
-            }
-        }
+    // if it starts with a double quote
+    if ($tokens[$stackPtr]['content']{0} ==='"') {
+
+      // ...and contains a single quote, we're good.
+      if (strpos($tokens[$stackPtr]['content'] , "'")) {
+        return;
+      }
+      // ...and contains an escaped character
+      if (
+        strpos($tokens[$stackPtr]['content'] , '\n') ||
+        strpos($tokens[$stackPtr]['content'] , '\t') ||
+        strpos($tokens[$stackPtr]['content'] , '\\') ||
+        strpos($tokens[$stackPtr]['content'] , '\v') ||
+        strpos($tokens[$stackPtr]['content'] , '\a')
+      ) {
+        return;
+      }
+
+      // Otherwise, it's an error.
+      $error = 'Only use Double Quotes when a variable is being ' .
+      'inserted.';
+      $data = [trim($tokens[$stackPtr]['content'])];
+
+      $phpcsFile->addError(
+        $error,
+        $stackPtr,
+        'FOUND',
+        $data
+      );
     }
+  }
+}
 
 }
