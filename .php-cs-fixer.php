86caf8927b56d0c7d1e9a7dbf4cb0d3910f32038
michaelw-lw <michaelwedderburn@nexcess.net>
2024-11-27 07:29:41 -0800

    MAD-14978 Apply PSR12 coding standard (#31)
    Update to PSR12
    
    * - Updated README.md
    - Changed to PSR12
    - Added PSR12 Sniffs as warnings for visibility
    
    * Updated PSR12 implementation
    
    * Added implementation of ecs "Easy Coding Standard" fixers into php-cs-fixer
    - Added Symfony DI
    - Added Multiline Array fixer
    - Added rule exceptions
    
    * Added custom repo for ecs to composer.json
    
    * - Added forked repo
    
    * - Removed php-cs-fixer from repo
    - Removed not needed code
    
    * - Added fixed for function parameter alignment
    
    * - Added new nocworx coding standard repo
    - Updated Fixer name
    
    * - Added package to configure installed paths
    - Updated fixer config
    
    * - Updates for PSR12
    
    ---------
    
    Co-authored-by: Michael Wedderburn <mdk999@gmail.com>

---
+++ .php-cs-fixer.php
@@ -0,0 +1,75 @@
+<?php
+
+ini_set('memory_limit', -1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symplify\CodingStandard\Fixer\ArrayNotation\StandaloneLineInMultilineArrayFixer;
+
+$container = new ContainerBuilder();
+$phpLoader = new PhpFileLoader($container, new FileLocator());
+$instanceof = [];
+$configurator = new ContainerConfigurator($container, $phpLoader, $instanceof, __DIR__, __FILE__);
+$services = $configurator->services();
+$services
+    ->defaults()
+    ->autowire()
+    ->autowire()
+    ->autoconfigure()
+    ->load('Symplify\CodingStandard\\', __DIR__ . '/vendor/symplify/coding-standard/src/*')
+    ->load('PhpCsFixer\\', __DIR__ . '/vendor/friendsofphp/php-cs-fixer/src/*')
+    ->set(StandaloneLineInMultilineArrayFixer::class)
+    ->public(true)
+;
+$container->compile();
+
+$finder = (new Finder())
+    ->in(__DIR__)
+;
+
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->registerCustomFixers([
+        $container->get(StandaloneLineInMultilineArrayFixer::class),
+    ])
+    ->setRules(
+        [
+            '@PSR12' => true,
+            '@PhpCsFixer' => true,
+            'blank_line_after_opening_tag' => true,
+            'linebreak_after_opening_tag' => true,
+            'ordered_imports' => ['sort_algorithm' => 'alpha'],
+            'no_unused_imports' => true,
+            'unary_operator_spaces' => true,
+            'binary_operator_spaces' => true,
+            'no_superfluous_phpdoc_tags' => false,
+            'phpdoc_no_package' => false,
+            'phpdoc_no_empty_return' => false,
+            'phpdoc_no_useless_inheritdoc' => false,
+            'concat_space' => ['spacing' => 'one'],
+            'method_argument_space' => [
+                'on_multiline' => 'ensure_fully_multiline',
+            ],
+            'control_structure_braces' => true,
+            'clean_namespace' => true,
+            'single_import_per_statement' => true,
+            'single_line_after_imports' => true,
+            'array_indentation' => true,
+            'array_syntax' => ['syntax' => 'short'],
+            'backtick_to_shell_exec' => true,
+            'no_extra_blank_lines' => true,
+            'single_space_around_construct' => true,
+            'indentation_type' => true,
+            'no_trailing_whitespace' => true,
+            'method_chaining_indentation' => true,
+            'Nocworx/codingstandard' => true,
+            'phpdoc_to_param_type' => false,
+            'phpdoc_to_property_type' => false,
+            'phpdoc_to_return_type' => false,
+        ]
+    )
+    ->setFinder($finder);
